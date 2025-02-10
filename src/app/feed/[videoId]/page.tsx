f218d93b8923edc9392b7fe3ff942a12f389750c
xminh <kirito14042003@gmail.com>
2025-02-10 23:08:45 +0700

    create project and setup

---
+++ src/app/feed/[videoId]/page.tsx
@@ -0,0 +1,10 @@
+interface PageProps {
+  params: Promise<{ videoId: string }>;
+}
+
+const Page = async ({ params }: PageProps) => {
+  const { videoId } = await params;
+  return <div>Video id : {videoId}</div>;
+};
+
+export default Page;
