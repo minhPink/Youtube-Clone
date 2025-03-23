fb5ea6c862c2e236f12f814ca8d0193e5848c0c1
xminh <kirito14042003@gmail.com>
2025-03-23 20:56:00 +0700

    procedures suggestions

--- src/modules/videos/ui/sections/suggestions-section.tsx
+++ src/modules/videos/ui/sections/suggestions-section.tsx
@@ -1,3 +1,21 @@
-export const SuggestionSection = () => {
-  return <div>Suggest Section</div>;
+"use client";
+
+import { DEFAULT_LIMIT } from "@/constants";
+import { trpc } from "@/trpc/client";
+
+interface SuggestionSectionProps {
+  videoId: string;
+}
+
+export const SuggestionSection = ({ videoId }: SuggestionSectionProps) => {
+  const [suggestions] = trpc.suggestions.getMany.useSuspenseInfiniteQuery(
+    {
+      videoId,
+      limit: DEFAULT_LIMIT,
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    }
+  );
+  return <div>{JSON.stringify(suggestions.pages[0].items[0])}</div>;
 };
